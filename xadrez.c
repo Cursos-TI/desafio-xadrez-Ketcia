d028fa8e253dbf025909e0a7ac36a9c47d2d92ca
Leticia Rafael <85456049+Ketcia@users.noreply.github.com>
2024-09-29 21:27:47 +0000

    Nivel Novato

--- xadrez.c
+++ xadrez.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 
 // Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
@@ -8,15 +9,155 @@ int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    const int bispoMove = 5;
+    const int torreMove = 5;
+    const int rainhaMove = 8;
+    char peca = 'n'; //b = bispo, t = torre, r = rainha
+    int direcao = 0;
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    printf("---- XADREZ ----\n\n");
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    do{
+        printf("[B] Bispo\n");
+        printf("[T] Torre\n");
+        printf("[R] Rainha\n");
+        printf("Qual peça voce deseja movimentar:");
+        scanf(" %c", &peca);
+        peca = toupper(peca);
 
+    }while (peca != 'B' && peca != 'T' && peca != 'R');
+    
+
+    if (peca == 'B')
+    {
+        do{
+
+            printf(" \n--- DIRECÕES ---\n");
+            printf("[1] Diagonal superior esquerda\n");
+            printf("[2] Diagonal superior direita\n");
+            printf("[3] Diagonal inferior direita\n");
+            printf("[4] Diagonal inferior esquerda\n");
+
+            printf("Para qual direção voce deseja movimentar o BISPO:");
+            scanf("%d", &direcao);
+
+        }while (direcao < 1 && direcao > 4);
+
+        if (direcao = 1)
+        {
+            for (int i = 0; i < bispoMove; i++)
+            {
+                printf("Esquerda\n");
+                printf("Cima\n");
+            }
+            
+        }else if (direcao = 2)
+        {
+            for (int i = 0; i < bispoMove; i++)
+            {
+                printf("Direita\n");
+                printf("Cima\n");
+            }
+        }
+        else if (direcao = 2)
+        {
+            for (int i = 0; i < bispoMove; i++)
+            {
+                printf("Direita\n");
+                printf("Baixo\n");
+            }
+        }else{
+            for (int i = 0; i < bispoMove; i++)
+            {
+                printf("Esquerda\n");
+                printf("Baixo\n");
+            }
+        }
+        
+        
+    }else if (peca == 'T')
+    {
+        do{
+            printf(" \n--- DIRECÕES ---\n");
+            printf("[1] Esquerda\n");
+            printf("[2] Direita\n");
+            printf("[3] Cima\n");
+            printf("[4] Baixo\n");
+
+            printf("Para qual direção voce deseja movimentar a TORRE: ");
+
+            scanf("%d", &direcao);
+
+        }while (direcao < 1 && direcao > 4);
+
+        if (direcao = 1)
+        {
+            for (int i = 0; i < torreMove; i++)
+            {
+                printf("Esquerda\n");
+            }
+            
+        }else if (direcao = 2)
+        {
+            for (int i = 0; i < torreMove; i++)
+            {
+                printf("Direita\n");
+            }
+        }
+        else if (direcao = 2)
+        {
+            for (int i = 0; i < torreMove; i++)
+            {
+                printf("Cima\n");
+            }
+        }else{
+            for (int i = 0; i < torreMove; i++)
+            {
+                printf("Baixo\n");
+            }
+        }
+    }else{
+        do{
+            printf(" \n--- DIRECÕES --- \n");
+            printf("[1] Esquerda\n");
+            printf("[2] Direita\n");
+            printf("[3] Cima\n");
+            printf("[4] Baixo\n");
+
+            printf("Para qual direção voce deseja movimentar a RAINHA:");
+
+            scanf("%d", &direcao);
+
+        }while (direcao < 1 && direcao > 4);
+
+        if (direcao = 1)
+        {
+            for (int i = 0; i < rainhaMove; i++)
+            {
+                printf("Esquerda\n");
+            }
+            
+        }else if (direcao = 2)
+        {
+            for (int i = 0; i < rainhaMove; i++)
+            {
+                printf("Direita\n");
+            }
+        }
+        else if (direcao = 2)
+        {
+            for (int i = 0; i < rainhaMove; i++)
+            {
+                printf("Cima\n");
+            }
+        }else{
+            for (int i = 0; i < rainhaMove; i++)
+            {
+                printf("Baixo\n");
+            }
+        }
+    }
+   
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
